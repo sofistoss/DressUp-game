8ad57828aefeff7cab78687c642b563dcb36dc00
sofistoss <sofiano.a@hotmail.fr>
2020-01-28 19:51:18 +0100

    new graphic
    3d

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ function init()
     state.i=1;
     state.j=1;
     state.k=1;
-    
+
 }
 
 var state = {
@@ -37,7 +37,7 @@ function nextdress()
     dress.setAttribute("class","dress3");
          state.i=0;
     }
-    
+
 }
 
 function nextshoe()
@@ -61,18 +61,18 @@ function nextshoe()
     shoe.setAttribute("class","shoe3");
          state.j=0;
     }
-    
+
 }
 
 function nexthair()
 {
     console.log("inside function nexthair");
-    
+
     console.log(state.k);
     var hairf=document.getElementById("hairfront");
     var hairb=document.getElementById("hairback");
     hairb.setAttribute("class","hairback");
-    
+
     if(state.k===0){
     hairf.setAttribute("class","hairfront1");
         state.k++;
@@ -89,5 +89,5 @@ function nexthair()
     hairf.setAttribute("class","hairfront3");
          state.k=0;
     }
-    
+
 }
